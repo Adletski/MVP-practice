5690126ae11745a0202274bb27959a74a1b4ab5a
Adlet <adlet.zh19966@gmail.com>
2023-03-30 10:34:40 +0600

    Initial Commit

---
+++ MVP2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP2
+//
+//  Created by Adlet Zhantassov on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
